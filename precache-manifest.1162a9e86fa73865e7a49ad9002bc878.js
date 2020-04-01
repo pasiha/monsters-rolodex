3e74bd0e6e075f2be046fe17e1b2278b1cba919f
Pasi Haavisto <pasi.haavisto@dimedia.fi>
2020-04-01 12:19:25 +0300

    Updates

---
+++ precache-manifest.1162a9e86fa73865e7a49ad9002bc878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbd6004ac4a4425e700720d6ed80d15",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a3086934959548b95f2b",
+    "url": "/monsters-rolodex/static/css/main.6ea8a6ed.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3086934959548b95f2b",
+    "url": "/monsters-rolodex/static/js/main.d3785fb5.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
